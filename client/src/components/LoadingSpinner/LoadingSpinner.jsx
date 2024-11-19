7b2d8b8161d7f3b34e104e6f087e323fccc204b1
Dimitar Dimitrov <dimitardimitrov0317@gmail.com>
2024-11-19 17:20:33 +0200

    loading spinner updated

--- client/src/components/LoadingSpinner/LoadingSpinner.jsx
+++ client/src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -2,9 +2,7 @@ import styles from './LoadingSpinner.module.css';
 
 export default function LoadingSpinner() {
     return (
-        <div className={styles.spinner}>
-            <div className={styles.loading}></div>
-        </div>
+        <span class={styles.loader}></span>
     );
 };
 
