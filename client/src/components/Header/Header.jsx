00a53fc482d0ea0bbb08873b0980b19b326139d6
Dimitar Dimitrov <dimitardimitrov0317@gmail.com>
2024-11-21 12:44:55 +0200

    icon updated

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -30,7 +30,7 @@ export default function Header() {
     return (
         <div className={styles.container}>
             <div className={styles.logo} onClick={() => {navigate('/home')}}>
-                <img src="/trip-assistant-logo.png" alt="Trip Assistant Logo" />
+                <img src="/flight.png" alt="Trip Assistant Logo" />
                 <div className={styles.title}>AI Trip Assistant</div>
             </div>
             <div className={styles.navigation}>
