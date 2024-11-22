ec8b9fb7feba132a63117b2f7de3433e714df1fd
Dimitar Dimitrov <dimitardimitrov0317@gmail.com>
2024-11-22 16:49:47 +0200

    responsiveness implemented

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -6,17 +6,17 @@ export default function Home() {
             <div className="generate-trip">
                 <h2>Plan a trip</h2>
                 <p>Need help planning your next adventure? Let our AI-powered assistant give you accommodation,food and tourist recommendations that are matching your preferences and budget!</p>
-                <a href="/trip-generator">Start planning your trip now!</a>
+                <a href="/trip-generator">Start planning your trip now</a>
             </div>
             <div className="generate-location">
                 <h2>Can't decide on a destination?</h2>
                 <p>Get exciting destination suggestions tailored to your interests, budget, and travel dates.</p>
-                <a href="/location-suggestions">Let us recommend!</a>
+                <a href="/location-suggestions">Let us recommend</a>
             </div>
             <div className="schedule-trip">
                 <h2>Already made some plans for a trip but want to make the most of it?</h2>
                 <p>We will create an approximate schedule of what to do during your stay in a certain location, helping you maximize your time and experiences.</p>
-                <a href="/trip-scheduler">Create a detailed schedule!</a>
+                <a href="/trip-scheduler">Create a detailed schedule</a>
             </div>
         </div>
 
