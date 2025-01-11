bed01261dad9f532cca7d86a8c2acfc0bd17f592
Dimitar Dimitrov <dimitardimitrov0317@gmail.com>
2025-01-11 14:05:03 +0200

    hamburger menu for mobile

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import styles from './Header.module.css';
 import UserContext from '../../context/UserContext';
 import { useNavigate } from 'react-router-dom';
@@ -6,7 +6,9 @@ import { useNavigate } from 'react-router-dom';
 export default function Header() {
     const { user } = useContext(UserContext);
     const navigate = useNavigate();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     let initials = '';
+
     if (user) {
         let firstLetter = user.full_name.slice(0, 1);
         let firstLetterSurname = user.full_name.split(' ')[1].slice(0, 1);
@@ -17,8 +19,8 @@ export default function Header() {
         event.preventDefault();
         try {
             const response = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/logout`, {
-                method: 'POST', 
-                credentials: 'include'
+                method: 'POST',
+                credentials: 'include',
             });
             if (response.ok) {
                 navigate('/');
@@ -32,20 +34,42 @@ export default function Header() {
 
     return (
         <div className={styles.container}>
-            <div className={styles.logo} onClick={() => {navigate('/home')}}>
+            <div className={styles.logo} onClick={() => navigate('/home')}>
                 <img src="/flight.png" alt="Trip Assistant Logo" />
                 <div className={styles.title}>AI Trip Assistant</div>
             </div>
-            <div className={styles.navigation}>
-                {user && (
-                    <>
-                        <a href="/home" className={styles.home}>Home</a>
-                        <a href={`/my-plans/${user.id}`} className={styles.mytrips}>Plans</a>
-                        <a href="" onClick={handleLogout} className={styles.logout}>Logout</a>
-                        <div className={styles.profile}>{initials}</div>
-                    </>
-                )}
-            </div>
+            {user && (
+                <div className={styles.right}>
+                    <div
+                        className={`${styles.navigation} ${
+                            isMenuOpen ? styles.showMenu : ''
+                        }`}
+                    >
+                        <>
+                            <a href="/home" className={styles.home}>Home</a>
+                            <a
+                                href={`/my-plans/${user.id}`}
+                                className={styles.mytrips}
+                            >
+                                Plans
+                            </a>
+                            <a
+                                href=""
+                                onClick={handleLogout}
+                                className={styles.logout}
+                            >
+                                Logout
+                            </a>
+                        </>
+                    </div>
+                    <div
+                        className={styles.profile}
+                        onClick={() => setIsMenuOpen(!isMenuOpen)}
+                    >
+                        {initials}
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
